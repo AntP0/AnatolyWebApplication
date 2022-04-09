daae4bd23d5924a3cfae7b59218521e45bba2302
AntP0 <frosia-ksiysha@mail.ru>
2022-04-09 16:55:50 +0300

    Добавьте файлы проекта.

---
+++ Anatoly(ZAO TB)WebApplication/Pages/Crystallization.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Anatoly_ZAO_TB_WebApplication.Pages
+{
+    public class CrystallizationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
